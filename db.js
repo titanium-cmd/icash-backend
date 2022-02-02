6efabc68049163a23bc24fb8f74d6a82c5da3c70
titanium <titaniumdev123@gmail.com>
2022-02-02 11:34:49 +0000

    mongo db uri dynamic

--- db.js
+++ db.js
@@ -1,9 +1,12 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 
+
 const connectDB = async () => {
+  const env = process.env.NODE_ENV || 'development';
+  const db_uri = env === 'development' ? process.env.LOCAL_DB_URI : process.env.ATLAS_DB_URI
   try {    
-    await mongoose.connect(process.env.LOCAL_DB_URI);
+    await mongoose.connect(db_uri);
     console.log('DB connected');
   } catch (error) {
     console.log('DB not connected ', error.message);
