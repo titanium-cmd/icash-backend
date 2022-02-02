af9aceb1f3c5d721900c3d9f638e444078a6c3e6
titanium <titaniumdev123@gmail.com>
2022-02-02 11:39:46 +0000

    transaction model modified

--- models/transaction.js
+++ models/transaction.js
@@ -1,20 +1,20 @@
 const mongoose = require('mongoose');
 
 const transactionSchema = new mongoose.Schema({
-  transaction_type: {
+  type: {
     type: String,
     required: [true, 'Transaction type is required'],
   },
-  transaction_mode: {
+  mode: {
     type: String,
     required: [true, 'Transaction mode is required'],
   },
-  transaction_amount: {
+  amount: {
     type: Number,
     min: [0, 'Transaction amount cant be negative'],
     required: [true, 'Transaction amount is required']
   },
-  transaction_description: {
+  description: {
     type: String,
     required: [true, 'Transaction description is required']
   },
